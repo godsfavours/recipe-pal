a04baa2406f5ce1d9226424e6bf96720823bac9f
godsfavours <godsfavourcs@gmail.com>
2023-05-12 15:23:48 -0700

    create header and shopping list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule

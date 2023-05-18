aa1fd02c40670d063e5a0a03bc3de8cd4e230596
godsfavours <godsfavourcs@gmail.com>
2023-05-18 14:17:12 -0700

    implement displaying single recipe items

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is just a test recipe.', 'https://senterrealtors.com/files/2022/04/favorite-restaurants-1024x684.jpg'),
+    new Recipe('Test Recipe', 'This is just a test recipe.', 'https://senterrealtors.com/files/2022/04/favorite-restaurants-1024x684.jpg'),
+  ];
 
+
+  ngOnInit() {
+
+  }
 }
